693bbe898d142e5019b550581cd92779d159c795
saini21199 <sachinsainiandromeda007@gmail.com>
2021-05-12 15:11:16 +0530

    performed file and directory operations

---
+++ readdirectories/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace readdirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> lst = new List<string>() { 
+            //"Ritik",
+            //"sandeep",
+            //"Ram",
+            //"saumya",
+            //"Sachin"
+            //};
+
+            //string s="";
+            //lst.ForEach(i => s = s + i); 
+
+
+            //File.Create("abc.txt");
+            ////Console.WriteLine(File.ReadAllText("abc.txt"));
+            //File.WriteAllText("abc.txt", "Overwriting the file");
+            //File.AppendAllText("abc.txt", "\nAdding new List to file");
+            //File.AppendAllText("abc.txt", s);
+            //Console.WriteLine(File.ReadAllText("abc.txt"));
+            //var x = File.ReadLines("abc.txt").ToList();
+
+            //x.ForEach(i => Console.WriteLine(i));
+            //Console.WriteLine(x.Count);
+
+            //using (var x = File.OpenText("abc.txt")) {
+            //    Console.WriteLine(x.ReadToEnd());
+            //}
+
+
+            
+
+            //Directory.CreateDirectory("input");
+            //Directory.CreateDirectory("output");
+
+            //File.Copy("abc.txt", @"C:\Users\lenovo\source\repos\readdirectories\" +
+            //    @"readdirectories\bin\Debug\netcoreapp3.1\input\input.txt");
+
+            //File.Move("abc.txt", @"C:\Users\lenovo\source\repos\readdirectories\" +
+            //    @"readdirectories\bin\Debug\netcoreapp3.1\output\abc.txt");
+
+            //var x  = new DirectoryInfo()
+            //string currentDire= Directory.GetCurrentDirectory();
+           // var alldirectory = Directory.GetDirectories(currentDire);
+            //foreach (string al in alldirectory)
+            //    Console.WriteLine(al);
+
+            //Directory.Delete(alldirectory[1], true);
+
+            //foreach (string fi in Directory.GetFiles(alldirectory[0]))
+            //    Console.WriteLine(fi);
+
+            //Directory.EnumerateDirectories(currentDire).ToList().ForEach(i =>
+            //Console.WriteLine(i));
+
+            //Console.WriteLine(File.GetAttributes(@"C:\Users\lenovo\source\repos\readdirectories\" +
+            //    @"readdirectories\bin\Debug\netcoreapp3.1\input\input.txt"));
+
+            //File.SetAttributes(@"C:\Users\lenovo\source\repos\readdirectories\readdirectories\bin\Debug\netcoreapp3.1\input\input.txt", FileAttributes.Compressed);
+            //File.Encrypt(@"C:\Users\lenovo\source\repos\readdirectories\readdirectories\bin\Debug\netcoreapp3.1\input\input.txt");
+        }
+    }
+}
